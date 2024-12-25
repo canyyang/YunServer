856fb192893a894f98af93974a3517d3444c31c7
canyyang <canyangchen@126.com>
2024-12-25 16:23:57 +0800

    ADD pm2 config

--- config/config.local.js
+++ config/config.local.js
@@ -1,7 +1,13 @@
 exports.mongoose = {
   client: {
     url: 'mongodb://127.0.0.1:27017/egg-mongo',
-    options: {}
+    options: {
+      user: 'admin',         // 用户名
+      pass: 'canyyang',       // 密码
+      authSource: 'admin',    // 认证数据库，通常是 admin
+      useNewUrlParser: true,  // 推荐使用新的 URL 解析器
+      useUnifiedTopology: true // 推荐使用新的拓扑结构
+    }
   }
 }
 
